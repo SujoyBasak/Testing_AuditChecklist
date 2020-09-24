6ccf3e507362e1f301d694ce689ea2a018991c40
SujoyBasak <sujoyr723@gmail.com>
2020-09-24 20:47:24 +0530

    Testing Modified

--- AuditChecklistControllerTest.cs
+++ AuditChecklistControllerTest.cs
@@ -4,45 +4,97 @@ using AuditChecklistModule.Providers;
 using NUnit.Framework;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Moq;
+using AuditChecklistModule.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AuditChecklistModule.Testing
 {
+    
     public class AuditChecklistControllerTest
     {
-                
-        [TestCase("Internal")]
-        [TestCase("SOX")]        
-        public void GetQuestionsTest(string type)
-        {
-            ChecklistRepo rep = new ChecklistRepo();
-            ChecklistProvider pr = new ChecklistProvider(rep);
-            AuditChecklistController con = new AuditChecklistController(pr);
+        List<Questions> ls = new List<Questions>();
+        [SetUp]
+        public void setup()
+        {   
+            
+            ls=new List<Questions>()
+            {
+                new Questions
+                {
+                    QuestionNo=1,
+                    Question="Have all Change requests followed SDLC before PROD move?"
+                },
+                new Questions
+                {
+                    QuestionNo=2,
+                    Question="Have all Change requests been approved by the application owner?"
+                }
+            };
 
-            OkObjectResult data = con.GetQuestions(type) as OkObjectResult;
-            Assert.AreEqual(200, data.StatusCode);
+           
         }
 
 
+        [TestCase("Internal")]
+        [TestCase("SOX")]
+        public void CorrectTypeTestProvider(string type)
+        {
+            
+            Mock<IChecklistRepo> mock = new Mock<IChecklistRepo>();
+            mock.Setup(p => p.GetQuestions(type)).Returns(ls);
+            ChecklistProvider cp = new ChecklistProvider(mock.Object);
+            List<Questions> result= cp.QuestionsProvider(type);
+            Assert.AreEqual(2,result.Count);
+        }
 
-        [TestCase("SOX123")]
         [TestCase("Internal123")]
-        public void GetQuestionsFailTest(string type)
+        [TestCase("SOX123")]
+        public void WrongTypeTestProvider(string type)
         {
             try
             {
-                ChecklistRepo rep = new ChecklistRepo();
-                ChecklistProvider pr = new ChecklistProvider(rep);
-                AuditChecklistController con = new AuditChecklistController(pr);
-
-                OkObjectResult data = con.GetQuestions(type) as OkObjectResult;
-
-                Assert.AreEqual(200, data.StatusCode);
+                Mock<IChecklistRepo> mock = new Mock<IChecklistRepo>();
+                mock.Setup(p => p.GetQuestions(type)).Returns(ls);
+                ChecklistProvider cp = new ChecklistProvider(mock.Object);
+                List<Questions> result = cp.QuestionsProvider(type);
+                Assert.AreEqual(2, result.Count);
             }
             catch(Exception e)
             {
                 Assert.AreEqual("Object reference not set to an instance of an object.",e.Message);
             }
             
+        }
+
+        [TestCase("Internal")]
+        [TestCase("SOX")]
+        public void CorrectTypeController(string type)
+        {
+            Mock<IChecklistProvider> mock = new Mock<IChecklistProvider>();
+            mock.Setup(p => p.QuestionsProvider(type)).Returns(ls);
+            AuditChecklistController cp = new AuditChecklistController(mock.Object);
+            OkObjectResult result = cp.GetQuestions(type) as OkObjectResult;
+            Assert.AreEqual(200, result.StatusCode);
+        }
+
+        [TestCase("Internal123")]
+        [TestCase("SOX123")]
+        public void WrongTypeController(string type)
+        {
+            try
+            {
+                Mock<IChecklistProvider> mock = new Mock<IChecklistProvider>();
+                mock.Setup(p => p.QuestionsProvider(type)).Returns(ls);
+                AuditChecklistController cp = new AuditChecklistController(mock.Object);
+                OkObjectResult result = cp.GetQuestions(type) as OkObjectResult;
+                Assert.AreEqual(200, result.StatusCode);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual("Object reference not set to an instance of an object.", e.Message);
+            }
             
         }
 
